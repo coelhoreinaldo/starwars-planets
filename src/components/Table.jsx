41308d5b8591212d9d4848274fba2403dc5ff244
coelhoreinaldo <scoelhor7@gmail.com>
2023-05-02 17:14:03 -0300

    ✨ feat: req 2

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
-// import AppContext from '../contexts/AppContext';
+import React, { useContext } from 'react';
+import AppContext from '../contexts/AppContext';
 import useFetch from '../hooks/useFetch';
 
 const API_URL = 'https://swapi.dev/api/planets';
 
 function Table() {
   const { apiData } = useFetch(API_URL);
+  const { planetName, setPlanetName } = useContext(AppContext);
 
+  const handleChange = ({ target }) => {
+    const { value } = target;
+
+    setPlanetName(value);
+  };
   // simplesmente usar um objectVALUES/KEYS NAO FUNCIONAAAAAAAAA
   // console.log(Object.values(apiData[0]));
-
-  console.log(apiData);
-  console.log('xd');
   return (
     <div>
+      <label data-testid="name-filter" htmlFor="name-filter">
+        <input
+          type="text"
+          id="name-filter"
+          value={ planetName }
+          onChange={ handleChange }
+          placeholder="Search by name"
+        />
+      </label>
       <table border="1">
         <thead className="has-background-primary">
           <tr>
@@ -36,23 +48,25 @@ function Table() {
           </tr> */}
         </thead>
         <tbody>
-          {apiData.map((element) => (
-            <tr key={ element.name }>
-              <td>{element.name}</td>
-              <td>{element.rotation_period}</td>
-              <td>{element.orbital_period}</td>
-              <td>{element.diameter}</td>
-              <td>{element.climate}</td>
-              <td>{element.gravity}</td>
-              <td>{element.terrain}</td>
-              <td>{element.surface_water}</td>
-              <td>{element.films}</td>
-              <td>{element.population}</td>
-              <td>{element.created}</td>
-              <td>{element.edited}</td>
-              <td>{element.url}</td>
-            </tr>
-          ))}
+          {apiData
+            .filter((e) => e.name.toLowerCase().includes(planetName.toLowerCase()))
+            .map((element) => (
+              <tr key={ element.name }>
+                <td>{element.name}</td>
+                <td>{element.rotation_period}</td>
+                <td>{element.orbital_period}</td>
+                <td>{element.diameter}</td>
+                <td>{element.climate}</td>
+                <td>{element.gravity}</td>
+                <td>{element.terrain}</td>
+                <td>{element.surface_water}</td>
+                <td>{element.films}</td>
+                <td>{element.population}</td>
+                <td>{element.created}</td>
+                <td>{element.edited}</td>
+                <td>{element.url}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
--- src/contexts/AppProvider.js
+++ src/contexts/AppProvider.js
@@ -2,13 +2,12 @@ import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import AppContext from './AppContext';
 
-const INITIAL_STATE = { nome: 'teste', idade: 27 };
 function AppProvider({ children }) {
-  const [state1, setState1] = useState(INITIAL_STATE);
+  const [planetName, setPlanetName] = useState('');
 
   const values = useMemo(() => ({
-    state1, setState1,
-  }), [state1, setState1]);
+    planetName, setPlanetName,
+  }), [planetName, setPlanetName]);
 
   return (
     <AppContext.Provider value={ values }>
